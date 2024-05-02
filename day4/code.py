748776a63b31c1a3c5272b07eb7dc5fd78f81140
Utkarsh Raj Sinha <ur5441@srmist.edu.in>
2024-05-02 11:04:17 +0530

    Update: day4 code optimised and updated

--- day4/code.py
+++ day4/code.py
@@ -1,4 +1,4 @@
-#Leetcode Solution
+#Leetcode Solution (Old)
 
 # class Solution:
 #     def halvesAreAlike(self, s: str) -> bool:
@@ -34,15 +34,89 @@
 #         else:
 #             return False
 
+#Leetcode Solution (New)
 
-#An altered solution for dry run.
+# class Solution:
+#     def halvesAreAlike(self, s: str) -> bool:
+#         a=s[:int(len(s)/2)]
+#         b=s[int(len(s)/2):]
+#         a_vowel=0
+#         b_vowel=0
+#         for i,j in zip(a,b):
+#             if i=='a' or i=='A':
+#                 a_vowel=a_vowel+1
+#             elif i=='e' or i=='E':
+#                 a_vowel=a_vowel+1
+#             elif i=='i' or i=='I':
+#                 a_vowel=a_vowel+1
+#             elif i=='o' or i=='O':
+#                 a_vowel=a_vowel+1
+#             elif i=='u' or i=='U':
+#                 a_vowel=a_vowel+1
+                
+#             if j=='a' or j=='A':
+#                 b_vowel=b_vowel+1
+#             elif j=='e' or j=='E':
+#                 b_vowel=b_vowel+1
+#             elif j=='i' or j=='I':
+#                 b_vowel=b_vowel+1
+#             elif j=='o' or j=='O':
+#                 b_vowel=b_vowel+1
+#             elif j=='u' or j=='U':
+#                 b_vowel=b_vowel+1
+#         if a_vowel==b_vowel:
+#             return True
+#         else:
+#             return False
+
+
+#An altered solution for dry run(Old).
+
+# def halvesAreAlike(s):
+#     a=s[:int(len(s)/2)]
+#     b=s[int(len(s)/2):]
+#     a_vowel=0
+#     b_vowel=0
+#     for i in a:
+#         if i=='a' or i=='A':
+#             a_vowel=a_vowel+1
+#         elif i=='e' or i=='E':
+#             a_vowel=a_vowel+1
+#         elif i=='i' or i=='I':
+#             a_vowel=a_vowel+1
+#         elif i=='o' or i=='O':
+#             a_vowel=a_vowel+1
+#         elif i=='u' or i=='U':
+#             a_vowel=a_vowel+1
+    
+#     for j in b:
+#         if j=='a' or j=='A':
+#             b_vowel=b_vowel+1
+#         elif j=='e' or j=='E':
+#             b_vowel=b_vowel+1
+#         elif j=='i' or j=='I':
+#             b_vowel=b_vowel+1
+#         elif j=='o' or j=='O':
+#             b_vowel=b_vowel+1
+#         elif j=='u' or j=='U':
+#             b_vowel=b_vowel+1
+#     if a_vowel==b_vowel:
+#         return True
+#     else:
+#         return False
+
+# s=input('Enter a string with equal length: ')
+# result=halvesAreAlike(s)
+# print(result)
+
+#An altered solution for dry run(New).
 
 def halvesAreAlike(s):
     a=s[:int(len(s)/2)]
     b=s[int(len(s)/2):]
     a_vowel=0
     b_vowel=0
-    for i in a:
+    for i,j in zip(a,b):
         if i=='a' or i=='A':
             a_vowel=a_vowel+1
         elif i=='e' or i=='E':
@@ -53,8 +127,7 @@ def halvesAreAlike(s):
             a_vowel=a_vowel+1
         elif i=='u' or i=='U':
             a_vowel=a_vowel+1
-    
-    for j in b:
+            
         if j=='a' or j=='A':
             b_vowel=b_vowel+1
         elif j=='e' or j=='E':
--- day4/day4_readme.md
+++ day4/day4_readme.md
@@ -14,9 +14,11 @@ Algorithm used:
 
 First the string is split into two words using string slicing. The, two loops are run through each slice and it counts the number of vowels in both of them and store them in two variables. It then compares both variables and returns `True` if they are equal else `False`.
 
+Update: Code optimised a bit. Upon learning about `zip()` function in python. The new code zips the two words together since multiple iterations can be done in a single loop. This can increase perform and will make code a bit more efficient.
+
 Code Performance:
 
-Runtime: 41ms
-Memory: 16.66MB
+Runtime: 41ms -> 39ms
+Memory: 16.66MB -> 16.64MB
 
 NOTE: These performance results are for one time run and may give different stats for different inputs and runs.
